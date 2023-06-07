6565d306f0a922d607258d859c9df738630413c0
mayramartins <mayramartins.in@gmail.com>
2023-06-07 18:45:16 -0300

    wip: home UI improvements

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,8 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+
+import { Spinner, Card, FormEntry } from '../components';
 
 const Home = () => {
+  const [spinning, setSpinning] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+
   return (
-    <div>Home</div>
+    <section className="max-w-7x1 mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Community Creations </h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w [500px]">Explore a compilation of imaginative and visually captivating images created by AIsthetic</p>
+      </div>
+
+    </section>
   )
 }
 
