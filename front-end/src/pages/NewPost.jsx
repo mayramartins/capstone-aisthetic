d4cf74fc328caa69e0716151a449eabfe2e8bc95
mayramartins <mayramartins.in@gmail.com>
2023-06-07 20:50:12 -0300

    wip: fixings prompt logic

--- front-end/src/pages/NewPost.jsx
+++ front-end/src/pages/NewPost.jsx
@@ -2,12 +2,29 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { preview } from '../assets';
-import { getRamdomPrompt } from '../utilities';
+import { getRandomPrompt } from '../utilities';
 import { FormEntry, Spinner } from '../components';
 
 const NewPost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    picture: '',
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>NewPost</div>
+    <section className="max-w-7x1 mx-auto">
+
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w [500px]">Create imaginative and visually captivating images created with AIsthetic and share with the community!</p>
+      </div>
+
+    </section>
   )
 }
 
