b20d2c3d6f983eca060d7784d3b412e897117e0e
mayramartins <mayramartins.in@gmail.com>
2023-06-08 17:32:38 -0300

    wip: working on api key error

--- front-end/src/pages/NewPost.jsx
+++ front-end/src/pages/NewPost.jsx
@@ -17,9 +17,31 @@ const NewPost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/aisthetic', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
 
-  }
+        const data = await response.json();
+        
+        setForm({...form, picture: `data.image/jpeg;base64,${data.picture}`})
+      } catch (error) {
+        alert(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please enter a prompt')
+    }
+  };
+  
   
   const handleSubmit = () => {
 
