5e03162b0240dbdfecdc88564f66d6f6ba67e1f8
mayramartins <mayramartins.in@gmail.com>
2023-06-08 14:45:23 -0300

    wip: backend routes

--- back-end/routes/aistheticRoutes.js
+++ back-end/routes/aistheticRoutes.js
@@ -17,4 +17,25 @@ router.route("/").get((req, res) => {
   res.send("Hello from AIsthetic!");
 });
 
+// Route makes call openAI API
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    // get the image from API
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    // send image to forontend
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.status(200).json({ picture: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
+});
+
 export default router;
