3f04149be9486790af5a643924292195ae99d29c
mayramartins <mayramartins.in@gmail.com>
2023-06-08 00:03:24 -0300

    wip: form field implementation

--- front-end/src/components/FormEntry.jsx
+++ front-end/src/components/FormEntry.jsx
@@ -10,7 +10,26 @@ const FormEntry = ({ LabelName, type, name, placeholder, value, handleChange, is
         >
           {LabelName}
         </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise me
+          </button>
+        )}
       </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required 
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm round-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+      />
     </div>
   )
 }
