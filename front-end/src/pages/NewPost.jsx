c98fddf58fac3910fdabd63bc73b292c36fbfd31
mayramartins <mayramartins.in@gmail.com>
2023-06-11 13:31:46 -0300

    changes url/env

--- front-end/src/pages/NewPost.jsx
+++ front-end/src/pages/NewPost.jsx
@@ -21,7 +21,7 @@ const NewPost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/aisthetic', {
+        const response = await fetch('http://aisthetic.herokuapp.com/api/v1/aisthetic', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -53,7 +53,7 @@ const NewPost = () => {
     if(form.prompt && form.picture) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('http://aisthetic.herokuapp.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
